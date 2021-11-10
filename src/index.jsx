9a6dd2dcf3bf29e97d5247737338fb2ee9081e93
Alex Eskandarpour <aesshoferi@gmail.com>
2021-11-10 13:23:56 +0330

    fix: use helmet async to fix UNSAFE_componentWillMount

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { HelmetProvider } from 'react-helmet-async';
 import App from './App';
 import store from './store';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
