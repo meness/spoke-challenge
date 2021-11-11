7d2bb47fbe43bab47791cbf29fe595cc3451a268
Alex Eskandarpour <aesshoferi@gmail.com>
2021-11-11 11:30:18 +0330

    ui: align loading icon center and disable shrink

--- src/components/elements/CircularLoading.jsx
+++ src/components/elements/CircularLoading.jsx
@@ -1,7 +1,7 @@
 import { CircularProgress } from '@mui/material';
 
 const CircularLoading = () => {
-  return <CircularProgress />;
+  return <CircularProgress disableShrink sx={{ position: 'absolute', left: '50%', top: '50%' }} />;
 };
 
 CircularLoading.propTypes = {};
