e1a2ae6ebc44f18dcab77f3a7f3a9ab215d37a5c
Alex Eskandarpour <aesshoferi@gmail.com>
2021-11-11 11:52:58 +0330

    fix: cannot complete multiple tasks at a time

--- src/saga.js
+++ src/saga.js
@@ -1,10 +1,10 @@
-import { takeLatest } from 'redux-saga/effects';
+import { takeLatest, takeEvery } from 'redux-saga/effects';
 import { fetchTasksSaga, updateTaskStatusSaga } from './reducers/task/task.saga';
 import SAGA_ACTION from './saga.actions';
 
 function* mySaga() {
   yield takeLatest(SAGA_ACTION.FETCH_TASKS, fetchTasksSaga);
-  yield takeLatest(SAGA_ACTION.UPDATE_TASK_STATUS, updateTaskStatusSaga);
+  yield takeEvery(SAGA_ACTION.UPDATE_TASK_STATUS, updateTaskStatusSaga);
 }
 
 export default mySaga;
