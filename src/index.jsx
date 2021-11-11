a131db3a055d9345fc32969bdf462eee974db129
Alex Eskandarpour <aesshoferi@gmail.com>
2021-11-11 11:51:12 +0330

    ui: config custom mui theme

--- src/index.jsx
+++ src/index.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { HelmetProvider } from 'react-helmet-async';
+import { ThemeProvider } from '@mui/material';
 import App from './App';
 import store from './store';
+import theme from './theme';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <HelmetProvider>
-        <App />
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
       </HelmetProvider>
     </Provider>
   </React.StrictMode>,
---
+++ src/theme.js
@@ -0,0 +1,23 @@
+import { createTheme } from '@mui/material';
+
+const myTheme = createTheme({
+  palette: {
+    type: 'light',
+    primary: {
+      main: '#1DCC81',
+      contrastText: 'rgba(255,255,255,0.87)',
+    },
+    secondary: {
+      main: '#E0CA3C',
+    },
+  },
+  components: {
+    MuiButtonBase: {
+      defaultProps: {
+        disableRipple: true,
+      },
+    },
+  },
+});
+
+export default myTheme;
