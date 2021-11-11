af8035af00a2c25b89ae83be8bff62f1f6dbbeb8
Alex Eskandarpour <aesshoferi@gmail.com>
2021-11-11 11:33:35 +0330

    ui: remove default body margin

--- src/index.jsx
+++ src/index.jsx
@@ -2,14 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { HelmetProvider } from 'react-helmet-async';
+import { createTheme, ThemeProvider } from '@mui/material';
 import App from './App';
 import store from './store';
+
+const themeOptions = createTheme({
+  palette: {
+    type: 'light',
+    primary: {
+      main: '#1DCC81',
+      contrastText: 'rgba(255,255,255,0.87)',
+    },
+    secondary: {
+      main: '#E0CA3C',
+    },
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <HelmetProvider>
-        <App />
+        <ThemeProvider theme={themeOptions}>
+          <App />
+        </ThemeProvider>
       </HelmetProvider>
     </Provider>
   </React.StrictMode>,
