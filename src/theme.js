522c5faab4000d7ff4405ea20c535b8938b1ea45
Alex Eskandarpour <aesshoferi@gmail.com>
2021-11-13 22:24:16 +0330

    ui: change color theme to look like Todoist

--- src/theme.js
+++ src/theme.js
@@ -4,11 +4,32 @@ const myTheme = createTheme({
   palette: {
     type: 'light',
     primary: {
-      main: '#1DCC81',
-      contrastText: 'rgba(255,255,255,0.87)',
+      main: '#d94e44',
+      50: '#fcecef',
+      100: '#f9d0d4',
+      200: '#e7a0a0',
+      300: '#da7d7c',
+      400: '#e4625c',
+      500: '#e75646',
+      600: '#d94e44',
+      700: '#c7453e',
+      800: '#b94037',
+      900: '#a9382e',
     },
-    secondary: {
-      main: '#E0CA3C',
+    success: {
+      main: '#6fcf97',
+    },
+    error: {
+      main: '#eb5757',
+    },
+    warning: {
+      main: '#f2c94c',
+    },
+    info: {
+      main: '#2f80ed',
+    },
+    action: {
+      main: '#184faf',
     },
   },
 });
