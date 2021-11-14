cda4ac32f8102b56480cf89f3afd5d723c916bd2
Alex Eskandarpour <aesshoferi@gmail.com>
2021-11-14 11:42:05 +0330

    fix: improve performance by memorizing table columns

--- src/pages/tasks/TasksPage.jsx
+++ src/pages/tasks/TasksPage.jsx
@@ -3,7 +3,7 @@ import { Helmet } from 'react-helmet-async';
 import { Box, Tooltip, Paper, Button } from '@mui/material';
 import { Title, CheckCircle, Edit } from '@mui/icons-material';
 import { useNavigate } from 'react-router-dom';
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import ToggleTaskStatus from '../../components/elements/ToggleTaskStatus';
 import IconicText from '../../components/elements/IconicText';
 import useTasks from '../../hooks/UseTasks';
@@ -14,46 +14,49 @@ const TasksPage = () => {
   const navigate = useNavigate();
   const [tablePageSize, setTablePageSize] = useState(10);
   const [isAddNewTaskModalOpen, setIsAddNewTaskModalOpen] = useState(false);
-  const columns = [
-    {
-      field: 'status',
-      type: 'boolean',
-      headerName: (
-        <Tooltip title="Status" placement="top">
-          <CheckCircle />
-        </Tooltip>
-      ),
-      width: 100,
-      headerAlign: 'center',
-      align: 'center',
-      valueGetter: ({ row }) => row.completed,
-      renderCell: ({ row }) => {
-        return <ToggleTaskStatus taskId={row.id} taskStatus={row.completed} />;
+  const columns = useMemo(
+    () => [
+      {
+        field: 'status',
+        type: 'boolean',
+        headerName: (
+          <Tooltip title="Status" placement="top">
+            <CheckCircle />
+          </Tooltip>
+        ),
+        width: 100,
+        headerAlign: 'center',
+        align: 'center',
+        valueGetter: ({ row }) => row.completed,
+        renderCell: ({ row }) => {
+          return <ToggleTaskStatus taskId={row.id} taskStatus={row.completed} />;
+        },
       },
-    },
-    {
-      field: 'title',
-      headerName: <IconicText icon={Title} text="Task" />,
-      flex: 1,
-      sortable: false,
-    },
-    {
-      field: 'actions',
-      type: 'actions',
-      getActions: (params) => [
-        <GridActionsCellItem
-          icon={<Edit />}
-          onClick={() => navigate(`${params.id}`, { replace: false })}
-          label="Edit"
-          color="action"
-        />,
-      ],
-      width: 100,
-      sortable: false,
-      headerAlign: 'center',
-      align: 'center',
-    },
-  ];
+      {
+        field: 'title',
+        headerName: <IconicText icon={Title} text="Task" />,
+        flex: 1,
+        sortable: false,
+      },
+      {
+        field: 'actions',
+        type: 'actions',
+        getActions: (params) => [
+          <GridActionsCellItem
+            icon={<Edit />}
+            onClick={() => navigate(`${params.id}`, { replace: false })}
+            label="Edit"
+            color="action"
+          />,
+        ],
+        width: 100,
+        sortable: false,
+        headerAlign: 'center',
+        align: 'center',
+      },
+    ],
+    [navigate],
+  );
 
   return (
     <>
